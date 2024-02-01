a9c323778ec3ad8f42f926c0ffa5da68300f9180
JhussyaraReis <147738753+JhussyaraReis@users.noreply.github.com>
2024-01-31 23:00:11 -0300

    alterações poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,6 +3,22 @@ const limit = 6;
 
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
+function converterPokemon(pokeDetalhado) {
+  const pokemon = new Pokemon();
+  pokemon.nome = pokeDetalhado.name;
+  pokemon.ordem = pokeDetalhado.order;
+  const tipos = pokeDetalhado.types.map((typesSlot) => typesSlot.type.name);
+  const [tipoPrincipal] = tipos;
+  pokemon.tipoPrincipal = tipoPrincipal;
+  pokemon.altura = pokeDetalhado.height;
+  pokemon.largura = pokeDetalhado.width;
+  pokemon.peso = pokeDetalhado.Weight;
+  pokemon.categoria = pokeDetalhado.order;
+  pokemon.tipos = tipos;
+  pokemon.img1 = pokeDetalhado.sprites.other.dream_world.front_default;
+  return pokemon;
+}
+
 fetch(url)
   .then((response) => response.json())
   .then((resJson) => {
@@ -11,8 +27,8 @@ fetch(url)
       fetch(elemento.url)
         .then((response) => response.json())
         .then((pokeDetalhado) => {
-          // criar função exibir detalhes
           criarCardPokemons(pokeDetalhado);
+          console.log(converterPokemon(pokeDetalhado));
         });
     });
   })
---
+++ assets/js/pokemon.js
@@ -0,0 +1,12 @@
+class Pokemon {
+  nome;
+  ordem;
+  tipos;
+  tipoPrincipal;
+  img1;
+  altura;
+  largura;
+  peso;
+  categoria;
+  habilidades;
+}
